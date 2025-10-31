d885dbbca5d9e0572c032a5116d560e4ec6576c2
Grant Lemons <grantlemons@aol.com>
2025-10-31 16:26:31 -0600

    rework find and implement insert

--- src/contree.rs
+++ src/contree.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use bytemuck::{Pod, Zeroable};
 use glam::{IVec3, UVec3, Vec3};
 
@@ -20,18 +21,22 @@ struct ContreeInner {
     children: [u32; 64],
 }
 
+type ChildIndex = usize;
+type Addr = usize;
+
 #[derive(Debug, Clone, Default)]
 struct Contree {
-    root: usize,
+    center_offset: Vec3,
+    root: Addr,
     /// Distance from center to face
     size: u32,
     inners: Vec<ContreeInner>,
     leaves: Vec<ContreeLeaf>,
-    inner_tombstones: Vec<usize>,
-    leaf_tombstones: Vec<usize>,
+    inner_tombstones: Vec<Addr>,
+    leaf_tombstones: Vec<Addr>,
 }
 
-fn morton_code(p: UVec3) -> u64 {
+fn morton_code(norm_p: UVec3) -> u64 {
     fn interleave(val: u32) -> u64 {
         // Magic number bit-fuckery
         let magic_numbers = [
@@ -48,84 +53,216 @@ fn morton_code(p: UVec3) -> u64 {
         }
         return x;
     }
-    (interleave(p.x) << 2) | (interleave(p.y) << 1) | interleave(p.z)
+    (interleave(norm_p.x) << 2) | (interleave(norm_p.y) << 1) | interleave(norm_p.z)
+}
+
+struct FindResult {
+    leaf_address: Option<Addr>,
+    traversal_stack: Vec<ChildIndex>,
+    parent_addrs: Vec<Addr>,
 }
 
 impl Contree {
-    fn normalize(&self, p: Vec3) -> Vec3 {
-        p + Vec3::splat(self.size as f32)
+    fn normalize(&self, p: Vec3) -> UVec3 {
+        (p - self.center_offset + Vec3::splat(self.size as f32))
+            .round()
+            .as_uvec3()
     }
 
-    fn svo_abs(v: i32) -> i32 {
-        if v < 0 { -v - 1 } else { v }
+    fn svo_abs(v: f32) -> f32 {
+        if v < 0. { -v - 1. } else { v }
     }
 
-    fn in_bounds(self, p: IVec3) -> bool {
-        (p.map(Self::svo_abs).max_element() as u32) < self.size
+    fn in_bounds(&self, p: Vec3) -> bool {
+        ((p - self.center_offset)
+            .map(Self::svo_abs)
+            .round()
+            .max_element() as u32)
+            < self.size
+    }
+
+    fn new_inner_node(&mut self, parent: Addr, child: ChildIndex) -> Addr {
+        let new_node = ContreeInner {
+            contains: 0,
+            leaf: 0,
+            light: 0,
+            children: [0; 64],
+        };
+        let addr = match self.inner_tombstones.pop() {
+            Some(addr) => {
+                self.inners[addr] = new_node;
+                addr
+            }
+            None => {
+                self.inners.push(new_node);
+                self.inners.len() - 1
+            }
+        };
+        self.inners[parent].children[child] = addr as u32;
+        self.update_parent_bitflags(parent, child, true, false, false);
+        addr
+    }
+
+    fn new_leaf_node(&mut self, parent: Addr, child: ChildIndex) -> Addr {
+        let new_node = ContreeLeaf {
+            contains: 0,
+            light: 0,
+            children: [0; 64],
+        };
+        let addr = match self.leaf_tombstones.pop() {
+            Some(addr) => {
+                self.leaves[addr] = new_node;
+                addr
+            }
+            None => {
+                self.leaves.push(new_node);
+                self.leaves.len() - 1
+            }
+        };
+        self.inners[parent].children[child] = addr as u32;
+        self.update_parent_bitflags(parent, child, true, true, false);
+        addr
     }
 
     fn update_parent_bitflags(
         &mut self,
-        parent: usize,
-        child_num: usize,
+        parent: Addr,
+        child: ChildIndex,
         exists: bool,
         leaf: bool,
         light: bool,
     ) {
-        let mask = (1 as u64) << child_num;
+        let mask = (1 as u64) << child;
         self.inners[parent].contains &= !mask;
-        self.inners[parent].contains |= (exists as u64) << child_num;
+        self.inners[parent].contains |= (exists as u64) << child;
         self.inners[parent].leaf &= !mask;
-        self.inners[parent].leaf |= (leaf as u64) << child_num;
+        self.inners[parent].leaf |= (leaf as u64) << child;
         self.inners[parent].light &= !mask;
-        self.inners[parent].light |= (light as u64) << child_num;
+        self.inners[parent].light |= (light as u64) << child;
     }
 
-    fn find(&self, normalized_pos: Vec3) -> (Option<usize>, usize, Vec<usize>) {
-        fn to_base_64(code: u64) -> Vec<usize> {
-            let mut digits = Vec::new();
-            let mut n = code;
-            if n == 0 {
-                digits.push(0);
-                return digits;
+    fn insert(&mut self, pos: Vec3, material: u8) -> Vec<Addr> {
+        if self.in_bounds(pos) {
+            let FindResult {
+                leaf_address,
+                mut traversal_stack,
+                mut parent_addrs,
+            } = self.find(pos, &[]);
+            match leaf_address {
+                Some(addr) => {
+                    let leaf = self
+                        .leaves
+                        .get_mut(addr)
+                        .expect("Leaf node does not exist!");
+
+                    let child_index = *traversal_stack.last().unwrap();
+                    leaf.children[child_index] = material;
+                    leaf.contains |= 1 << child_index;
+                }
+                None => {
+                    let (leaf_addr, child_index) =
+                        self.add_parents(&mut traversal_stack, &mut parent_addrs);
+
+                    let leaf = self
+                        .leaves
+                        .get_mut(leaf_addr)
+                        .expect("Leaf node does not exist!");
+
+                    leaf.children[child_index] = material;
+                    leaf.contains |= 1 << child_index;
+                }
             }
-            while n != 0 {
-                digits.push((n as usize) & 0b111111);
-                n >>= 6;
+            return parent_addrs;
+        } else {
+            // TODO: Expand contree
+            let contains = self.inners[self.root].contains;
+            todo!()
+        }
+    }
+
+    fn add_parents(
+        &mut self,
+        traversal_stack: &mut Vec<ChildIndex>,
+        parent_addrs: &mut Vec<Addr>,
+    ) -> (Addr, ChildIndex) {
+        let mut leaf_addr = 0;
+        for (i, child_index) in traversal_stack.clone().iter().enumerate().rev() {
+            let parent: Addr = *parent_addrs.last().expect("No root!");
+            match i {
+                0 => return (leaf_addr, *child_index),
+                1 => leaf_addr = self.new_leaf_node(parent, *child_index),
+                _ => parent_addrs.push(self.new_inner_node(parent, *child_index)),
             }
-            digits.reverse();
-            digits
+        }
+        unreachable!("Never reached bottom of traversal stack!")
+    }
+
+    fn to_base_64(code: u64) -> Vec<ChildIndex> {
+        let mut digits = Vec::new();
+        let mut n = code;
+        if n == 0 {
+            digits.push(0);
+            return digits;
+        }
+        while n != 0 {
+            digits.push((n as ChildIndex) & 0b111111);
+            n >>= 6;
+        }
+        digits
+    }
+
+    fn find(&self, pos: Vec3, given_parent_addrs: &[Addr]) -> FindResult {
+        let code = morton_code(self.normalize(pos));
+        let mut traversal_stack = Self::to_base_64(code);
+
+        for i in given_parent_addrs {
+            traversal_stack.pop();
         }
 
-        let code = morton_code(normalized_pos.as_uvec3());
-        let traversal_indexes = to_base_64(code);
-
-        let mut parent_indexes = Vec::new();
+        let mut parent_addrs = given_parent_addrs.to_vec();
         if self.inners.is_empty() {
-            return (None, *traversal_indexes.first().unwrap(), parent_indexes);
+            return FindResult {
+                leaf_address: None,
+                traversal_stack,
+                parent_addrs,
+            };
         }
 
-        parent_indexes.push(self.root);
+        parent_addrs.push(self.root);
         let mut current = self.inners[self.root];
-        for i in 0..(traversal_indexes.len()) {
-            let index = traversal_indexes[i];
-            let child_addr = current.children[index] as usize;
+        for i in 0..(traversal_stack.len()) {
+            let index = traversal_stack.last().unwrap();
+            let child_addr = current.children[*index] as Addr;
 
             let child_exists = current.contains & (1 << index) != 0;
             let child_leaf = current.leaf & (1 << index) != 0;
 
             if child_exists && child_leaf {
-                return (Some(child_addr), traversal_indexes[i + 1], parent_indexes);
+                traversal_stack.pop();
+                return FindResult {
+                    leaf_address: Some(child_addr),
+                    traversal_stack,
+                    parent_addrs,
+                };
             }
             if child_exists {
-                parent_indexes.push(child_addr);
+                traversal_stack.pop();
+                parent_addrs.push(child_addr);
                 current = self.inners[child_addr];
             } else {
-                return (None, index, parent_indexes);
+                return FindResult {
+                    leaf_address: None,
+                    traversal_stack,
+                    parent_addrs,
+                };
             }
         }
 
-        (None, *traversal_indexes.first().unwrap(), parent_indexes)
+        FindResult {
+            leaf_address: None,
+            traversal_stack,
+            parent_addrs,
+        }
     }
 }
 
@@ -133,7 +270,7 @@ impl Contree {
 mod tests {
     use glam::{UVec3, Vec3};
 
-    use crate::contree::{Contree, ContreeInner, ContreeLeaf, morton_code};
+    use crate::contree::{Contree, ContreeInner, ContreeLeaf, FindResult, morton_code};
 
     #[test]
     fn morton_code_example() {
@@ -142,12 +279,26 @@ mod tests {
     }
 
     #[test]
+    fn morton_code_zero() {
+        let code = morton_code(UVec3::new(0, 0, 0));
+        let index = Contree::to_base_64(code);
+        assert_eq!(code, 0);
+        assert_eq!(index, &[0]);
+    }
+
+    #[test]
     fn traverse_empty() {
         let p = Vec3::new(5., 8., 9.);
         let contree = Contree::default();
-        let (child_addr, _next_index, path) = contree.find(contree.normalize(p));
-        assert!(child_addr.is_none());
-        assert!(path.is_empty());
+        let FindResult {
+            leaf_address,
+            traversal_stack,
+            parent_addrs,
+        } = contree.find(p, &[]);
+
+        assert!(leaf_address.is_none());
+        assert_eq!(traversal_stack, &[5, 28]);
+        assert!(parent_addrs.is_empty());
     }
 
     #[test]
@@ -175,9 +326,42 @@ mod tests {
             ..Default::default()
         };
 
-        let (child_addr, next_index, path) = contree.find(contree.normalize(p));
-        assert_eq!(child_addr, Some(0));
-        assert_eq!(next_index, 0);
-        assert!(matches!(path.as_slice(), &[0]));
+        let FindResult {
+            leaf_address,
+            traversal_stack,
+            parent_addrs,
+        } = contree.find(p, &[]);
+
+        assert_eq!(leaf_address, Some(0));
+        assert_eq!(traversal_stack.as_slice(), &[0]);
+        assert_eq!(parent_addrs.as_slice(), &[0]);
+    }
+
+    #[test]
+    fn insert_traverse_tiny() {
+        let p = Vec3::new(0., 0., 0.);
+        let mut contree = Contree {
+            root: 0,
+            size: 16,
+            inners: vec![ContreeInner {
+                contains: 0,
+                leaf: 0,
+                light: 0,
+                children: [0; 64],
+            }],
+            leaves: Vec::new(),
+            ..Default::default()
+        };
+        contree.insert(p, 10);
+
+        let FindResult {
+            leaf_address,
+            traversal_stack,
+            parent_addrs,
+        } = contree.find(p, &[]);
+
+        assert_eq!(leaf_address, Some(0));
+        assert_eq!(traversal_stack.as_slice(), &[0]);
+        assert_eq!(parent_addrs.as_slice(), &[0, 1]);
     }
 }
